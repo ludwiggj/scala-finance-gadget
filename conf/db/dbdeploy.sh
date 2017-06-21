10a04a6121c1b808b953fe68ac5d6956af030643
Graeme Ludwig <graeme.ludwig@btopenworld.com>
2017-06-21 22:24:53 +0100

    Dbdeploy now uses correct path info

--- conf/db/dbdeploy.sh
+++ conf/db/dbdeploy.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-USER_HOME=/Users/ludwiggj
+USER_HOME=~
 FINANCE_GADGET_HOME=${USER_HOME}/code/financeGadget
 PERSISTENCE_DB_HOME=${FINANCE_GADGET_HOME}/conf/db
+MYSQL_HOME=/usr/local/mysql
 
-mysql -h 127.0.0.1 -P 3306 -u root -v < ${PERSISTENCE_DB_HOME}/dropAndRecreateFinanceDatabase.sql
-mysql -h 127.0.0.1 -P 3306 -u root -v < ${PERSISTENCE_DB_HOME}/createFinanceBaseSchema.sql
+${MYSQL_HOME}/bin/mysql -h 127.0.0.1 -P 3306 -u root -v < ${PERSISTENCE_DB_HOME}/dropAndRecreateFinanceDatabase.sql
+${MYSQL_HOME}/bin/mysql -h 127.0.0.1 -P 3306 -u root -v < ${PERSISTENCE_DB_HOME}/createFinanceBaseSchema.sql
 
 cd ${PERSISTENCE_DB_HOME}
 
